6f70333f46283b3739c634904cb7df5dfc1f395c
mjc3bb <mjc3bb@gmail.com>
2019-02-13 11:38:23 -0500

    Added Automated testing

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -16,8 +16,10 @@ class Category(models.Model):
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
+        if self.views < 0:
+            self.views = 0
         self.slug = slugify(self.name)
-        super(Category, self).save(*args,**kwargs)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
